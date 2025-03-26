89a63cf5fd01759b91583af8327807d39846ef34
hm-faisal <contact.hmfaisal@gmail.com>
2025-03-26 23:15:47 +0600

    folder name update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/Components/Navber";
-import Sidebar from "@/Components/Sidebar";
+import Navbar from "@/components/Navber";
+import Sidebar from "@/components/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,14 +29,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className=" md:grid grid-cols-12">
-       <div className="col-span-3">
-       <Sidebar/>
-       </div>
-       <div className="col-span-9">
-        <Navbar/>
-       {children}
-       </div>
+        <div className="grid-cols-12 md:grid">
+          <div className="col-span-3">
+            <Sidebar />
+          </div>
+          <div className="col-span-9">
+            <Navbar />
+            {children}
+          </div>
         </div>
       </body>
     </html>
