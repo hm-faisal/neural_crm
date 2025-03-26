75b9e779bc1f7af4c341589e0fa8715723dd407e
mubashirbappi <mubashirbappi@gmail.com>
2025-03-26 16:35:44 +0600

    name update

--- Components/Sidebar.tsx
+++ Components/Sidebar.tsx
@@ -1,11 +1,12 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Sidebar = () => {
   return (
     <aside className="hidden md:flex flex-col w-64 h-screen px-5 py-8 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700">
-      <a href="#">
-        <img className="w-auto h-7" src="https://merakiui.com/images/logo.svg" alt="" />
-      </a>
+      <Link href="/">
+      Neural CRM
+      </Link>
 
       <div className="flex flex-col justify-between flex-1 mt-6">
         <nav className="flex-1 -mx-3 space-y-3">
